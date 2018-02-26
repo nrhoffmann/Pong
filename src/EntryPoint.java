adba3b57de69e080ed4fc7d122f6b34dc9fb2aa6
Nathan Hoffmann <nathanrosshoffmann@gmail.com>
2018-02-26 18:46:06 -0500

    add pong window

---
+++ src/EntryPoint.java
@@ -0,0 +1,7 @@
+import gui.Pong;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        new Pong();
+    }
+}
---
+++ src/gui/Pong.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class Pong extends JFrame {
+
+    public Pong() {
+        super("Pong");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+}
