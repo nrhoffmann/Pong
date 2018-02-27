1db190015ae0d32c7fb1b9a4230f75c89c82c7e2
Nathan Hoffmann <nathanrosshoffmann@gmail.com>
2018-02-27 10:51:36 -0500

    add phy package

---
+++ src/phy/Velocity.java
@@ -0,0 +1,31 @@
+package phy;
+
+import java.awt.*;
+
+public class Velocity {
+    private double theta;
+    private int speed;
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public Point delta() {
+        double x = speed * Math.sin(theta);
+        double y = speed * Math.cos(theta);
+
+        return new Point((int) x, (int) y);
+    }
+}
