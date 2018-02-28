303f094dea4513986213f8e3c7880324c791f24e
Menasheh Peromsik <menashehyperomsik@gmail.com>
2018-02-28 02:19:46 -0500

    add ball step and y bounce

--- src/io/github/nrhoffmann/pong/gui/Ball.java
+++ src/io/github/nrhoffmann/pong/gui/Ball.java
@@ -5,27 +5,63 @@ import io.github.nrhoffmann.pong.physics.Vector;
 import java.awt.*;
 
 public class Ball {
-    private Velocity velocity;
+    private Vector vector;
     private Point location;
+    private int size;
+    private Color color;
+    private int maxY;
+    private int minY;
 
-    public Velocity getVelocity() {
-        return velocity;
+    public void setColor(Color color) {
+        this.color = color;
     }
 
-    public void setVelocity(Velocity velocity) {
-        this.velocity = velocity;
-    }
-
-    public Point getLocation() {
-        return location;
+    public Color getColor() {
+        return color;
     }
 
     public void setLocation(Point location) {
         this.location = location;
     }
 
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setVector(Vector vector) {
+        this.vector = vector;
+    }
+
+    public Vector getVector() {
+        return vector;
+    }
+
+    public void step() {
+        int deltax = (int) vector.getSpeedx();
+        int deltay = (int) vector.getSpeedy();
+        if (location.y + deltay < minY || location.y + deltay > maxY) {
+            deltay = Math.max(deltay, minY - location.y); //todo this might be ugly, or it might simulate mass compression
+            deltay = Math.min(deltay, maxY - location.y);
+            vector.bounceY();
+        }
+        location.translate(deltax, deltay);
+    }
+
     Ball() {
-        Point delta = velocity.delta();
-        location.translate(delta.x, delta.y);
+        size = 50;
+        color = new Color(128, 128, 128);
+        location = new Point(GamePane.SIZE.width / 2, GamePane.SIZE.height / 2);
+        vector = new Vector();
+
+        maxY = GamePane.SIZE.height - size / 2;
+        minY = size / 2;
     }
 }
