a5de136fa6b9d597c1e5c766c15b28473a38e281
Nathan Hoffmann <nathanrosshoffmann@gmail.com>
2018-03-04 19:54:06 -0500

    paint only changed masks

--- src/io/github/nrhoffmann/pong/gui/GameObject.java
+++ src/io/github/nrhoffmann/pong/gui/GameObject.java
@@ -4,5 +4,6 @@ import java.awt.*;
 
 interface GameObject {
     void tick();
+    Rectangle changedMask();
     void paint(Graphics2D g2);
 }
--- src/io/github/nrhoffmann/pong/gui/GameTable.java
+++ src/io/github/nrhoffmann/pong/gui/GameTable.java
@@ -2,13 +2,16 @@ package io.github.nrhoffmann.pong.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 class GameTable extends JPanel {
     private final List<Ball> BALLS = new LinkedList<>();
     private final List<Paddle> LEFT_PADDLES = new LinkedList<>();
     private final List<Paddle> RIGHT_PADDLES = new LinkedList<>();
+    private final List<Rectangle> CHANGED_MASKS = new LinkedList<>();
 
     GameTable() {
         setBackground(Color.DARK_GRAY);
@@ -27,29 +30,26 @@ class GameTable extends JPanel {
     }
 
     private void apply() {
-        Graphics2D graphics = (Graphics2D) getGraphics();
-
-        paintBalls(graphics);
-        paintPaddles(graphics);
-    }
-
-    private void paintBalls(Graphics2D graphics) {
-        for (Ball ball : BALLS) {
-            ball.paint(graphics);
-        }
-    }
-
-    private void paintPaddles(Graphics2D graphics) {
         for (Paddle paddle : LEFT_PADDLES) {
-            paddle.paint(graphics);
+            CHANGED_MASKS.add(paddle.changedMask());
         }
 
         for (Paddle paddle : RIGHT_PADDLES) {
-            paddle.paint(graphics);
+            CHANGED_MASKS.add(paddle.changedMask());
+        }
+
+        for (Ball ball : BALLS) {
+            CHANGED_MASKS.add(ball.changedMask());
+        }
+
+        for (Rectangle changedMask : CHANGED_MASKS) {
+            repaint(changedMask);
         }
     }
 
     private void prepareNextTick() {
+        CHANGED_MASKS.clear();
+
         for (Ball ball : BALLS) {
             if (ball.getVector().getSpeedX() < 0) {
                 ball.setPotentiallyCollidablePaddles(LEFT_PADDLES);
@@ -68,5 +68,24 @@ class GameTable extends JPanel {
             LEFT_PADDLES.add(paddle);
         else if (paddle.SIDE == Side.RIGHT)
             RIGHT_PADDLES.add(paddle);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D graphics = (Graphics2D) g;
+
+        for (Paddle paddle : LEFT_PADDLES) {
+            paddle.paint(graphics);
+        }
+
+        for (Paddle paddle : RIGHT_PADDLES) {
+            paddle.paint(graphics);
+        }
+
+        for (Ball ball : BALLS) {
+            ball.paint(graphics);
+        }
     }
 }
