4b03dd7ec5bc84583a8942016dbcdf7c7438c46e
Menasheh Peromsik <menashehyperomsik@gmail.com>
2018-03-04 23:21:33 -0500

    staticify some fields and enforce paddle min and max y

--- src/io/github/nrhoffmann/pong/gui/Paddle.java
+++ src/io/github/nrhoffmann/pong/gui/Paddle.java
@@ -8,33 +8,25 @@ import java.awt.*;
 public class Paddle implements GameObject {
 
     final Side SIDE;
+
+    private static final int HEIGHT = 150;
+    private static final int WIDTH = 20;
+    private static final int MAX_Y = GamePane.GAME_TABLE_HEIGHT - HEIGHT;
+    private static final int MIN_Y = 0;
+
     private Controller input;
-    private Color color;
+    private static final Color color = Color.BLACK;
     private Point location;
     private Point oldLocation;
-    private Vector vector;
-    private int height;
-    private int width;
-    private int arc;
-    private int maxY;
-    private int minY;
+    private Vector vector = new Vector();
 
     Paddle(Side side, Controller controller) {
         SIDE = side;
-
         input = controller;
-        height = 200;
-        width = 50;
-        arc = 25;
-        color = Color.BLACK;
 
-        int padding = (int) (width * 0.75);
-        location = new Point(padding + (side == Side.LEFT ? 0 : ((GamePane.WIDTH - (padding * 2) - width))), GamePane.GAME_TABLE_HEIGHT / 2 - height / 2);
+        int padding = (int) (WIDTH * 0.75);
+        location = new Point(padding + (side == Side.LEFT ? 0 : ((GamePane.WIDTH - (padding * 2) - WIDTH))), GamePane.GAME_TABLE_HEIGHT / 2 - HEIGHT / 2);
 
-        vector = new Vector(0, 0);
-
-        maxY = GamePane.GAME_TABLE_HEIGHT - height;
-        minY = 0;
     }
 
     public Point getLocation() {
@@ -42,24 +34,33 @@ public class Paddle implements GameObject {
     }
 
     public Rectangle getRectangle() {
-        return new Rectangle(location.x, location.y, width, height);
+        return new Rectangle(location.x, location.y, WIDTH, HEIGHT);
     }
 
     @Override
     public void tick() {
         oldLocation = new Point(location);
 
-        //todo
+        int newY = location.y + input.nextInput();
+
+        if (newY < MIN_Y) {
+            newY = MIN_Y;
+        }
+        if (newY > MAX_Y) {
+            newY = MAX_Y;
+        }
+
+        location.y = newY;
     }
 
     @Override
     public Rectangle changedMask() {
-        return new Rectangle(oldLocation.x, oldLocation.y, width, height).union(getRectangle());
+        return new Rectangle(oldLocation.x, oldLocation.y, WIDTH, HEIGHT).union(getRectangle());
     }
 
     @Override
     public void paint(Graphics2D g2) {
         g2.setColor(color);
-        g2.fillRoundRect(location.x, location.y, width, height, arc, arc);
+        g2.fillRect(location.x, location.y, WIDTH, HEIGHT);
     }
 }
