987872d9f18809d7ca85829183cf77466faef83e
Menasheh Peromsik <menashehyperomsik@gmail.com>
2018-03-18 17:32:56 -0400

    add hiscores class

---
+++ src/io/github/nrhoffmann/pong/gui/Hiscores.java
@@ -0,0 +1,56 @@
+package io.github.nrhoffmann.pong.gui;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Hiscores {
+    private final String PATH = System.getenv("HOME") + ".config/pong/scores.dat";
+
+    private ArrayList<Element> hiscores = new ArrayList<>();
+
+    public Hiscores(){
+        System.out.println(PATH);
+        load();
+    }
+
+    public void load(){
+        FileInputStream fin = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(PATH));
+            try {
+                hiscores = (ArrayList<Element>) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            ois.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean add(Element element){
+        //return true if it works and false otherwise
+
+        return true;
+    }
+
+    public void save(){
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(PATH));
+            oos.writeObject(hiscores);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace(); // todo do something better?
+        }
+    }
+
+    class Element {
+        private String name;
+        private int score;
+        Element(String name, int score){
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+}
